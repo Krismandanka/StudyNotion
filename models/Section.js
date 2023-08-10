0a89de6e6f4a4716cdd3398f3c4ecd9b4b8c022b
Krismandanka <krismandanka@gmail.com>
2023-08-10 09:15:25 +0530

    few models

---
+++ models/Section.js
@@ -0,0 +1,23 @@
+
+
+
+
+const mongoose = require("mongoose");
+
+const sectionSchema = new mongoose.Schema({
+    sectionName:{
+        type:"String",
+    },
+    subSection:[
+        {
+            type:mongoose.Schema.Types.ObjectID,
+            require:true,
+            res:"SubSection",
+        },
+    ]
+
+
+
+})
+
+module.exports=mongoose.model("Section",sectionSchema);
